3f8dbb23cff0fb3806dff5c33717e17543502975
Vysakh A V <vysakhav@protonmail.com>
2026-07-18 11:15:34 +0100

    Use product specfic flag for XER2 to get the feature

--- source/HarvesterSsp/webpa_interface.c
+++ source/HarvesterSsp/webpa_interface.c
@@ -469,10 +469,7 @@ char * getDeviceMac()
 	char getList[256] = "Device.DPoE.Mac_address";
 	char* getList1[] = {"Device.DPoE.Mac_address"};
 #else
-#if defined (_HUB4_PRODUCT_REQ_) || defined(_SR300_PRODUCT_REQ_) || defined(_SCER11BEL_PRODUCT_REQ_) || defined(_SCXF11BFL_PRODUCT_REQ_)
-        char getList[256] = "Device.DeviceInfo.X_COMCAST-COM_WAN_MAC";
-        char* getList1[] = {"Device.DeviceInfo.X_COMCAST-COM_WAN_MAC"};
-#elif defined(PON_GATEWAY)  /* PON/Fiber based gateways needs to be use Device.DeviceInfo.X_COMCAST-COM_WAN_MAC */
+#if defined (_HUB4_PRODUCT_REQ_) || defined(_SR300_PRODUCT_REQ_) || defined(_SCER11BEL_PRODUCT_REQ_) || defined(_SCXF11BFL_PRODUCT_REQ_) || defined(_XER2_PRODUCT_REQ_)
         char getList[256] = "Device.DeviceInfo.X_COMCAST-COM_WAN_MAC";
         char* getList1[] = {"Device.DeviceInfo.X_COMCAST-COM_WAN_MAC"};
 #else
